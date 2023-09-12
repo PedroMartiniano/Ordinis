78278ae0c1dbc6c1ff2701da933c538c9014256d
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-09-11 23:51:09 -0300

    changed the database

--- prisma/migrations/20230907025122_optional_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `Manutencao` MODIFY `data_fim` DATETIME(3) NULL;
-
--- AlterTable
-ALTER TABLE `Patrimonio` MODIFY `data_saida` DATETIME(3) NULL,
-    MODIFY `resp_retirada` VARCHAR(191) NULL;
