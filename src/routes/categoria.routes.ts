2cfd754099b4bcdbeed52d667d24161a3605b09c
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-09-16 10:10:33 -0300

    editando permissões para rotas

--- src/routes/categoria.routes.ts
+++ src/routes/categoria.routes.ts
@@ -3,6 +3,7 @@ import { CreateCategoriaController } from "../modules/categoria/createCategoria/
 import { GetCategoriaByIdController } from "../modules/categoria/getCategoriaById/GetCategoriaByIdController";
 import { GetAllCategoriasController } from "../modules/categoria/getAllCategorias/GetAllCategoriasController";
 import { DeleteCategoriaController } from "../modules/categoria/deleteCategoria/DeleteCategoriaController";
+import { ensureAuthPermissao } from "../middlewares/ensureAuthPermissao";
 
 export const categoriaRoutes = Router()
 
@@ -11,16 +12,18 @@ const getCategoriaByIdController = new GetCategoriaByIdController
 const getAllCategoriasController = new GetAllCategoriasController
 const deleteCategoriaController = new DeleteCategoriaController
 
-categoriaRoutes.post('/create', (req, res, next) => {
-    createCategoriaController.handle(req, res, next)
-})
-
 categoriaRoutes.get('/get/:id', (req, res, next) => {
     getCategoriaByIdController.handle(req, res, next)
 })
 
 categoriaRoutes.get('/get-all', (req, res, next) => {
     getAllCategoriasController.handle(req, res, next)
+})
+
+categoriaRoutes.use(ensureAuthPermissao('ADMINISTRADOR'))
+
+categoriaRoutes.post('/create', (req, res, next) => {
+    createCategoriaController.handle(req, res, next)
 })
 
 categoriaRoutes.delete('/delete/:id', (req, res, next) => {
