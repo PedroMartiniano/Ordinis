f7b457af1e0deaceae293e06824d52e0eb46b0da
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-09-24 03:26:00 -0300

    changed the response from patrimonio routes

---
+++ src/types/patrimonio-names.d.ts
@@ -0,0 +1,16 @@
+export interface PatrimonioNamesProps {
+    id: string
+    placa: string
+    descricao: string
+    estado: 'EXECELENTE' | 'OTIMO' | 'REGULAR' | 'RUIM' | 'PESSIMO'
+    valor: number
+    origem: 'PREFEITURA' | 'NV'
+    status: number
+    data_entrada: Date
+    data_saida: Date | null
+    resp_entrega: string
+    resp_retirada: string | null
+    localizacao: {id: string, descricao: string}
+    categoria: {id: string, descricao: string}
+    id_usuario: string
+  }
